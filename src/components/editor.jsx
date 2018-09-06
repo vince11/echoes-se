e533aa555906cfb579cb335c8e39e7a06361e03c
vince11 <latvincent@gmail.com>
2018-09-06 16:31:47 +0100

    input handling

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -5,19 +5,38 @@ import units from "../resources/unitDB";
 class Editor extends Component {
 
     state = {
-        unitNames: units.map(unit => { return {key: unit.id, value: unit.name}})
+        unitNames: units.map(unit => { return {key: unit.id, value: unit.name}}),
+        saveFile: [],
+        hasInput: false
     }
     
     render(){
         return (
             <div>
-                <DropDown title="Unit: " options={this.state.unitNames}/>
-                <DropDown title="Held Item: " options={[]}/>
-                <DropDown title="Forge: " options={[]}/>
-                <DropDown title="Class: " options={[]}/>
+                <div>
+                    <DropDown title="Unit: " options={[]}/>
+                    <DropDown title="Held Item: " options={[]}/>
+                    <DropDown title="Forge: " options={[]}/>
+                    <DropDown title="Class: " options={[]}/>
+                </div>
+                <div>
+                    <input type="file" disabled={!this.state.hasInput ? "" : "disabled"} onChange={this.handleFileInput}/>
+                </div>
             </div>
+
         );
     };
+
+    handleFileInput = (event) => {
+        if(event.target.files[0] != null){
+            var fileReader = new FileReader();
+            fileReader.onload = () => {
+                const buffer = fileReader.result;
+                this.setState({saveFile: new Uint8Array(buffer), hasInput: true});
+            };
+            fileReader.readAsArrayBuffer(event.target.files[0]);
+        }
+    }
 
 }
 
