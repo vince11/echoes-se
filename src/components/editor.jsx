f07da639d4b08ed09182c2c95b647ea7fb775061
vince11 <latvincent@gmail.com>
2018-09-07 18:06:45 +0100

    updating dropdowns handler

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -11,7 +11,6 @@ class Editor extends Component {
         saveFile: [],
         hasInput: false,
         unitsCache: null,
-        bindEvents: false,
         dropDowns: [
             {
                 title: "Unit",
@@ -44,7 +43,7 @@ class Editor extends Component {
                         <DropDown 
                             key={dropdown.title} 
                             dropdown={dropdown}
-                            onChange={this.state.bindEvents ? this.handleDropDownChange : null}
+                            onChange={this.handleDropDownChange}
                         />
                     ))}
                 </div>
@@ -73,7 +72,7 @@ class Editor extends Component {
         const db = [
             this.loadUnits(),
             items.map(item => item.name).sort(),
-            [1,2,3,4,5],
+            [0,1,2,3,4,5],
             unitClasses.map(uc => uc.name).sort()
         ];
         var dropDowns = [];
@@ -102,19 +101,124 @@ class Editor extends Component {
     handleDropDownChange = (dropdown, event) => {
         var ddCopy = [...this.state.dropDowns];
         var index = ddCopy.indexOf(dropdown);
-        ddCopy[index] = {...dropdown};
-        ddCopy[index].selected = event.target.value;
-        if(index === 0 && event.target.value !== ""){
-            var copyDD = {...ddCopy[1]};
-            ddCopy[1] = copyDD;
-            var itemAddress = this.state.unitsCache.get(event.target.value).itemAddr;
-            var bytes = editorUtils.getByteArray(this.state.saveFile, itemAddress, 8);
-            var itemHex = editorUtils.byteArrayToHex(bytes);
+
+        if(index === 0){
+            this.populateDropDowns(ddCopy, event.target.value);
+        }
+        else if(index === 1){
+            this.updateItem(ddCopy, event.target.value);
+        }
+        else if(index === 2){
+            this.updateForge(ddCopy, event.target.value);
+        }
+        else if(index === 3){
+            this.updateClass(ddCopy, event.target.value);
+        }
+    }
+
+    populateDropDowns(dropdowns, unitName){
+
+        if(unitName !== ""){
+
+            dropdowns[0] = {...dropdowns[0]};
+            dropdowns[1] = {...dropdowns[1]};
+            dropdowns[2] = {...dropdowns[2]};
+            dropdowns[3] = {...dropdowns[3]};
+
+            var itemAddress = this.state.unitsCache.get(unitName).itemAddr;
+            var itemBytes = editorUtils.getByteArray(this.state.saveFile, itemAddress, 8);
+            var itemHex = editorUtils.byteArrayToHex(itemBytes);
             var item = items.find((e) => e.hex === itemHex);
-            ddCopy[1].selected = item.name;
+
+            var classAddress = this.state.unitsCache.get(unitName).classAddr;
+            var classBytes = editorUtils.getByteArray(this.state.saveFile, classAddress, 8);
+            var classHex = editorUtils.byteArrayToHex(classBytes);
+            var unitClass = unitClasses.find((e) => e.id === classHex);
+    
+            var forgeCount = "";
+            if(item.maxStars > 0){
+                dropdowns[2].options = [...Array(item.maxStars + 1).keys()];
+                forgeCount = this.state.saveFile[itemAddress - 1] >> 4;
+            }
+            else {
+                dropdowns[2].options = [];
+            }
+
+            dropdowns[0].selected = unitName;
+            dropdowns[1].selected = item.name;
+            dropdowns[2].selected = forgeCount;
+            dropdowns[3].selected = unitClass.name;
+        }
+        else {
+            dropdowns.forEach((e, i) => {
+                dropdowns[i] = {...dropdowns[i]};
+                dropdowns[i].selected = "";
+            });
         }
 
-        this.setState({dropDowns: ddCopy});
+        this.setState({dropDowns: dropdowns});
+
+    }
+
+    updateItem = (dropdowns, newItem) => {
+        if(newItem !== ""){
+            var item = items.find((e) => e.name === newItem);
+            var newItemHex = item.id + (item.isDLC ? "010008" : "000000") + item.hex;
+            var newItemBytes = editorUtils.hexToBytesArray(newItemHex);
+            var newItemStart = this.state.unitsCache.get(dropdowns[0].selected).itemAddr - 4;
+    
+            var updatedSaveFile = [...this.state.saveFile];
+            newItemBytes.forEach((e, i) => {
+                updatedSaveFile[newItemStart + i] = e;
+            });
+    
+            var forgeCount = "";
+            if(item.maxStars > 0){
+                dropdowns[2].options = [...Array(item.maxStars + 1).keys()];
+                forgeCount = 0
+            }
+            else{
+                dropdowns[2].options = [];
+            }
+    
+            dropdowns[1] = {...dropdowns[1]};
+            dropdowns[1].selected = newItem;
+            dropdowns[2] = {...dropdowns[2]};
+            dropdowns[2].selected = forgeCount;
+    
+            this.setState({dropDowns: dropdowns, saveFile: updatedSaveFile});
+        }
+    }
+
+    updateForge = (dropdowns, newForge) => {
+        if(newForge !== ""){
+            var forgeAddress = this.state.unitsCache.get(dropdowns[0].selected).itemAddr - 1;
+            var updatedSaveFile = [...this.state.saveFile];
+            updatedSaveFile[forgeAddress] = (updatedSaveFile[forgeAddress] & 0x0F) | (newForge << 4);
+    
+            dropdowns[2] = {...dropdowns[2]};
+            dropdowns[2].selected = newForge;
+    
+            this.setState({dropDowns: dropdowns, saveFile: updatedSaveFile});
+        }
+    }
+
+    updateClass = (dropdowns, newClass) => {
+        if(newClass !== ""){
+            var classAddr = this.state.unitsCache.get(dropdowns[0].selected).classAddr;
+            var unitClass = unitClasses.find(e => e.name === newClass);
+            var newClassBytes = editorUtils.hexToBytesArray(unitClass.id);
+
+            var updatedSaveFile = [...this.state.saveFile];
+            newClassBytes.forEach((e, i) => {
+                updatedSaveFile[classAddr + i] = e;
+            });
+
+            dropdowns[3] = {...dropdowns[3]};
+            dropdowns[3].selected = newClass;
+    
+            this.setState({dropDowns: dropdowns, saveFile: updatedSaveFile});
+        }
     }
 
 }
